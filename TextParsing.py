e35bb63d65224204f0f370857b9ca24e10d69960
Starr <89001004087896@FEDIDCARD.GOV>
2022-01-24 10:02:31 -0500

    reformatting parser

--- TextParsing.py
+++ TextParsing.py
@@ -6,28 +6,30 @@ import docx
 import pandas as pd
 
 
-#parses the text from PDFs and .docx files in a directory and exports them to a .csv
-#check the FileContent column of the csv to identify any errors and the file associated with them
+# parses the text from PDFs and .docx files in a directory and exports them to a .csv
+# check the FileContent column of the csv to identify any errors and the file associated with them
+
 def main():
-    #sets the path to the current directory. Run the script in the directory containing the files to be parsed or reset the path to the file location
+    # sets the path to the current directory.
+    # Run the script in the directory containing the files to be parsed or reset the path to the file location
     folder = './'
     
-    #creates a list of all .docx files in the directory
+    # creates a list of all .docx files in the directory
     word_docs = []
     word_docs += [file for file in os.listdir(folder) if file.endswith('.docx')]
     
-    #creates a list of all .pdf files in the directory
+    # creates a list of all .pdf files in the directory
     pdfs = []
     pdfs += [file for file in os.listdir(folder) if file.endswith('.pdf')]
     
-    text =[]
+    text = []
     for document in word_docs:
-        text += [reading_word_documents(document)]
+        text += [read_word_documents(document)]
         
     pdf_text = []
     for pdf in pdfs:
         try:
-            pdf_text += [reading_pdfs(pdf)]
+            pdf_text += [read_pdfs(pdf)]
         except:
             pdf_text += ['Error Reading PDF']
             
@@ -39,14 +41,14 @@ def main():
          'FileContent': content
         })
     
-    #outputs the file name and its content to a row in a csv. If there is no content and error message is inserted
+    # outputs the file name and its content to a row in a csv. If there is no content and error message is inserted
     output = df.fillna('Error Reading Text')
     output = pd.DataFrame(output)
-    output.to_csv('whitepapers_text.csv')
+    output.to_csv('white_papers_text.csv')
 
 
-#function parses text from docx files by paragraph and aggregates it
-def reading_word_documents(file_name):
+# function parses text from docx files by paragraph and aggregates it
+def read_word_documents(file_name):
     doc = docx.Document(file_name)
     
     completed_text = []
@@ -57,15 +59,13 @@ def reading_word_documents(file_name):
     return '\n' .join(completed_text)
 
 
-#function extracts text from PDFs
-def reading_pdfs(file_name):
+# function extracts text from PDFs
+def read_pdfs(file_name):
     read_pdf = PyPDF2.PdfFileReader(file_name)
     page = read_pdf.getPage(0)
     page_content = page.extractText()
     
     return page_content
-
-
 
 if __name__ == "__main__":
     main()
